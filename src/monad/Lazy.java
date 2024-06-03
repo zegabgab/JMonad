b9c7f9252703c928fa97491ee84f21b96b03807c
Gabriel Korherr <gabrielkorherr@gmail.com>
2024-06-03 17:58:40 +0200

    Made Lazy a class

--- src/monad/Lazy.java
+++ src/monad/Lazy.java
@@ -1,10 +1,21 @@
 package monad;
 
+import java.util.*;
 import java.util.function.*;
 
-public interface Lazy<T> {
-    static <T> Lazy<T> of(Supplier<? extends T> supplier) {
-        return new VeryLazy<>(supplier);
+final class Lazy<T> {
+    private T value = null;
+    private final Supplier<? extends T> supplier;
+
+    Lazy(Supplier<? extends T> supplier) {
+        this.supplier = Objects.requireNonNull(supplier);
     }
-    T get();
+
+    public static <T> Lazy<T> of(Supplier<? extends T> supplier) {
+        return new Lazy<>(supplier);
+    }
+
+    public T get() {
+        return value != null ? value : (value = supplier.get());
+    }
 }
