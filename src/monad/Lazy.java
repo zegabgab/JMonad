7d83b62b600d49d7ef8b14a55e5234e3ebe9b3d3
Gabriel Korherr <e11825320@student.tuwien.ac.at>
2024-06-10 00:11:45 +0200

    Documentation for Lazy

--- src/monad/Lazy.java
+++ src/monad/Lazy.java
@@ -3,6 +3,11 @@ package monad;
 import java.util.*;
 import java.util.function.*;
 
+/**
+ * Abstraction for lazy initialization of a non-null value.
+ *
+ * @param <T> the type of the value
+ */
 public final class Lazy<T> implements Supplier<T> {
     private final Supplier<? extends T> supplier;
     private T value = null;
@@ -11,10 +16,22 @@ public final class Lazy<T> implements Supplier<T> {
         this.supplier = Objects.requireNonNull(supplier);
     }
 
+    /**
+     * Creates a new lazy value.
+     *
+     * @param supplier the function used to calculate the value. This should not return null
+     * @return a fresh lazy value
+     * @param <T> the type of the encapsulated value
+     */
     public static <T> Lazy<T> of(Supplier<? extends T> supplier) {
         return new Lazy<>(supplier);
     }
 
+    /**
+     * Grabs the encapsulated value or calculates it if not already initialized.
+     *
+     * @return the contained value
+     */
     public T get() {
         return value != null ? value : (value = supplier.get());
     }
